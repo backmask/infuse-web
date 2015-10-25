a62c3a76fdc4f6a7bdd890a787c4f9f3417176a6
Thomas Delayen <thomas.delayen@backmask.fr>
2015-10-26 00:39:54 +0100

    Clean device overview

--- app/scripts/controllers/dashboard/devices.js
+++ app/scripts/controllers/dashboard/devices.js
@@ -27,7 +27,7 @@ angular.module('infuseWebApp')
 
     $scope.families = [
       { id: 'sensor', name: 'Sensor', icon: '<i class="fa fa-wifi"></i>' },
-      { id: 'sensor.temperature', name: 'Thermometer', icon: '<img src="images/thermometer.png" />' },
+      { id: 'sensor.temperature', name: 'Thermometer', icon: '<img class="icon" src="images/thermometer.png" />' },
       { id: 'controller', name: 'Controller', icon: '<i class="fa fa-gamepad"></i>' }
     ].map(fillDefaultIcon);
 
@@ -38,16 +38,16 @@ angular.module('infuseWebApp')
       { id: 'joystick', name: 'Joystick', icon: '<i class="fa fa-gamepad"></i>' },
       { msGroup: false },
       { name: '<b>IMU</b>', msGroup: true },
-      { id: 'acceleration', name: 'Accelerometer', icon: '<img src="images/accelerometer.png" />' },
-      { id: 'gyroscope', name: 'Gyroscope', icon: '<img src="images/gyroscope.png" />' },
+      { id: 'acceleration', name: 'Accelerometer', icon: '<img class="icon" src="images/accelerometer.png" />' },
+      { id: 'gyroscope', name: 'Gyroscope', icon: '<img class="icon" src="images/gyroscope.png" />' },
       { msGroup: false },
       { name: '<b>Telemetry</b>', msGroup: true },
       { id: 'battery', name: 'Battery', icon: '<i class="fa fa-battery-three-quarters"></i>' },
       { id: 'thrust', name: 'Thrust', icon: '<i class="fa fa-rocket"></i>' },
       { msGroup: false },
       { name: '<b>Weather</b>', msGroup: true },
-      { id: 'barometer', name: 'Barometer', icon: '<img src="images/barometer.png" />' },
-      { id: 'temperature', name: 'Temperature', icon: '<img src="images/thermometer.png" />' },
+      { id: 'barometer', name: 'Barometer', icon: '<img class="icon" src="images/barometer.png" />' },
+      { id: 'temperature', name: 'Temperature', icon: '<img class="icon" src="images/thermometer.png" />' },
       { msGroup: false }
     ].map(fillDefaultIcon);
 
@@ -103,6 +103,15 @@ angular.module('infuseWebApp')
 
     $scope.stopEdit = function() {
       $scope.editingDevice = false;
+    };
+
+    $scope.getFamilyIcon = function(familyId) {
+      for (var i = 0; i < $scope.families.length; ++i) {
+        if ($scope.families[i].id === familyId) {
+          return $scope.families[i].icon;
+        }
+      }
+      return '<i class="fa fa-question"></i>';
     };
 
     refreshDevices();
