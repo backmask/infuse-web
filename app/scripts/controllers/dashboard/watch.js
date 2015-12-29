cfe7363dfbcaf09eaf5c960d89082ce4ceb0128c
Thomas Delayen <thomas.delayen@backmask.fr>
2015-12-30 00:42:43 +0100

    Probe edition

--- app/scripts/controllers/dashboard/watch.js
+++ app/scripts/controllers/dashboard/watch.js
@@ -2,8 +2,17 @@
 
 angular.module('infuseWebApp')
   .controller('WatchCtrl', function ($scope, $q, gatewayManager, devices) {
+    $scope.editingProbes = false;
     $scope.watch = [];
+    $scope.probes = [];
     $scope.measurements = [];
+
+    var availableProbes = [
+      "MaxSpeed",
+      "MinSpeed",
+      "Max",
+      "Min"
+    ];
 
     var getMeasurements = function(measurementId) {
       return $scope.measurements.map(function(m) {
@@ -13,6 +22,12 @@ angular.module('infuseWebApp')
           return copy;
         }
         return m;
+      });
+    };
+
+    var getProbes = function(probeType) {
+      return availableProbes.map(function(p) {
+        return { name: p, id: p, ticked: p === probeType };
       });
     };
 
@@ -55,7 +70,8 @@ angular.module('infuseWebApp')
         val: {
           name: 'new.watch',
           measurementId: 0
-        }
+        },
+        measurements: getMeasurements()
       });
     };
 
@@ -77,8 +93,75 @@ angular.module('infuseWebApp')
         .finally(function() { w.loading = false; })
     };
 
+    $scope.deleteWatch = function(w) {
+      w.loading = true;
+      gatewayManager.getConnection().doRequest('/watch/timeseries/remove', w.val)
+        .then(function() { $scope.watch.splice($scope.watch.indexOf(w), 1); })
+        .finally(function() { w.loading = false; })
+    };
+
     $scope.isWatchModified = function(w) {
       return !angular.equals(w.val, w.original) && w.val.measurementId > 0;
+    };
+
+    $scope.editProbes = function(w) {
+      $scope.editingProbes = true;
+      $scope.selectedWatch = w.val;
+      $scope.refreshingProbes = true;
+      gatewayManager.getConnection().doRequest('/watch/timeseries/probe/get', { watchId: w.val.id })
+        .then(function(d) {
+          $scope.probes = d.data[w.val.id].map(function(p) {
+            var pp = angular.copy(p);
+            pp.watchId = w.val.id;
+            return {
+              val: angular.copy(pp),
+              original: pp,
+              probesType: getProbes(pp.type)
+            };
+          });
+        })
+        .finally(function() { $scope.refreshingProbes = false; });
+    };
+
+    $scope.newProbe = function() {
+      $scope.probes.push({
+        val: {
+          eventName: 'event.name',
+          type: 'Max',
+          threshold: 1.0,
+          watchId: $scope.selectedWatch.id
+        },
+        probesType: getProbes('Max')
+      });
+    };
+
+    $scope.refreshProbeValue = function(p) {
+      if (p.probeType.length > 0) {
+        p.val.probe = p.probeType[0].id;
+      } else {
+        p.val.probe = "";
+      }
+    };
+
+    $scope.isProbeModified = function(p) {
+      return !angular.equals(p.val, p.original);
+    };
+
+    $scope.saveProbe = function(p) {
+      p.loading = true;
+      gatewayManager.getConnection().doRequest('/watch/timeseries/probe/add', p.val)
+        .then(function(d) {
+          p.val = angular.copy(d.data[0]);
+          p.original = angular.copy(p.val);
+        })
+        .finally(function() { p.loading = false; })
+    };
+
+    $scope.deleteProbe = function(p) {
+      p.loading = true;
+      gatewayManager.getConnection().doRequest('/watch/timeseries/probe/remove', p.val)
+        .then(function() { $scope.probes.splice($scope.probes.indexOf(p), 1); })
+        .finally(function() { p.loading = false; })
     };
 
     $scope.$on('$destroy', gwListener.unsubscribe);
